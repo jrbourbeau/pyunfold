883a848326a4c77400f19c0dcd898619226256fc
James Bourbeau <jrbourbeau@gmail.com>
2018-04-03 16:08:31 -0500

    Adds new example test

--- pyunfold/tests/test_unfold.py
+++ pyunfold/tests/test_unfold.py
@@ -8,7 +8,7 @@ import pytest
 
 from .testing_utils import diagonal_response, triangular_response
 
-from pyunfold.unfold import iterative_unfold, unfold
+from pyunfold.unfold import iterative_unfold 
 
 
 @pytest.mark.parametrize('response_type', ['diagonal', 'triangular'])
@@ -88,6 +88,30 @@ def test_example():
     unfolded = iterative_unfold(data, data_err,
                                 response, response_err,
                                 efficiencies, efficiencies_err,
+                                return_iterations=True)
+
+    pd.testing.assert_frame_equal(unfolded, expected)
+
+
+def test_example_2():
+    here = os.path.abspath(os.path.dirname(__file__))
+    expected = pd.read_hdf(os.path.join(here, 'example_unfolding_2.hdf'))
+
+    # Run example case
+    data = [100, 150]
+    data_err = [10, 12.2]
+    response = [[0.8, 0.1],
+                [0.2, 0.9]]
+    response_err = [[0.01, 0.01],
+                    [0.01, 0.01]]
+    efficiencies = [0.4, 0.67]
+    efficiencies_err = [0.01, 0.01]
+    priors = [0.34, 1-0.34]
+    # Perform iterative unfolding
+    unfolded = iterative_unfold(data, data_err,
+                                response, response_err,
+                                efficiencies, efficiencies_err,
+                                priors=priors,
                                 return_iterations=True)
 
     pd.testing.assert_frame_equal(unfolded, expected)
