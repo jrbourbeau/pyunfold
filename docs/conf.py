1e6e37e6a45ed259002dabea8b43c3df2ce11a27
James Bourbeau <jrbourbeau@gmail.com>
2018-04-05 17:03:04 -0500

    Adds current version number to docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import pyunfold
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,9 +26,9 @@ copyright = u'2018, James Bourbeau, Zigfried Hampel-Arias'
 author = u'James Bourbeau, Zigfried Hampel-Arias'
 
 # The short X.Y version
-version = u''
+version = pyunfold.__version__
 # The full version, including alpha/beta/rc tags
-release = u''
+release = pyunfold.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -83,14 +85,13 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
