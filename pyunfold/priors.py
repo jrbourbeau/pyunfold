Adding uniform prior, and correcting Jeffreys prior calculation

pyunfold/priors.py
@@ -5,10 +5,22 @@ from six import string_types
 import pandas as pd
 
 
+def uniform_prior(norm, num_causes):
+    """Uniform Prior
+    """
+    # All bins are given equal probability.
+    # Most generic prior.
+    prior = norm * np.ones(num_causes) / num_causes
+    # Want to make sure prior is normalized (i.e. prior.sum() == 1)
+    prior = prior / prior.sum()
+
+    return prior
+
+
 def jeffreys_prior(norm, xarray):
     """Jeffreys Prior
     """
-    # All cause bins are given equal probability.
+    # All cause bins are given equal probability mass.
     # Best prior for x-ranges spanning decades.
     ln_factor = np.log(xarray[-1] / xarray[0])
     prior = norm / (ln_factor * xarray)
@@ -18,13 +30,13 @@ def jeffreys_prior(norm, xarray):
     return prior
 
 
-def setup_prior(priors='Jeffreys', num_causes=None, num_observations=None):
+def setup_prior(priors='Jeffreys', num_causes=None, num_observations=None, cause_axis=None):
     """Setup for prior array
 
     Parameters
     ----------
     priors : str or array_like, optional
-        Prior distribution to use (default is 'Jeffreys', will use Jeffreys
+        Prior distribution to use (default is 'Uniform', will use Uniform
         prior).
     num_causes : int, optional
         Number of cause bins. Only needed if priors='Jeffreys' (default is None).
@@ -37,11 +49,13 @@ def setup_prior(priors='Jeffreys', num_causes=None, num_observations=None):
     prior : numpy.ndarray
         Normalized prior distribution.
     """
-    if isinstance(priors, string_types) and priors.lower() == 'jeffreys':
-        assert num_causes is not None, 'num_causes must be specified for Jeffreys prior'
+    if isinstance(priors, string_types) and priors.lower() == 'uniform':
+        assert num_causes is not None, 'num_causes must be specified for uniform prior'
+        assert num_observations is not None, 'num_observations must be specified for uniform prior'
+        prior = uniform_prior(norm=num_observations, num_causes=num_causes)
+    elif isinstance(priors, string_types) and priors.lower() == 'jeffreys':
         assert num_observations is not None, 'num_observations must be specified for Jeffreys prior'
-        cause_bin_edges = np.arange(num_causes + 1, dtype=float)
-        cause_axis = (cause_bin_edges[1:] + cause_bin_edges[:-1]) / 2
+        assert cause_axis is not None, 'cause_axis must be specified for Jeffreys prior'
         prior = jeffreys_prior(norm=num_observations, xarray=cause_axis)
     elif isinstance(priors, (list, tuple, np.ndarray, pd.Series)):
         prior = np.asarray(priors)

pyunfold/unfold.py
@@ -12,8 +12,8 @@ from .callbacks import validate_callbacks, extract_regularizer
 
 def iterative_unfold(data=None, data_err=None, response=None,
                      response_err=None, efficiencies=None,
-                     efficiencies_err=None, priors='Jeffreys', ts='ks',
-                     ts_stopping=0.01, max_iter=100, cov_type='multinomial',
+                     efficiencies_err=None, priors='Uniform', causes=None,
+                     ts='ks', ts_stopping=0.01, max_iter=100, cov_type='multinomial',
                      return_iterations=False, callbacks=None):
     """Performs iterative Bayesian unfolding
 
@@ -35,9 +35,13 @@ def iterative_unfold(data=None, data_err=None, response=None,
         Uncertainties of detection efficiencies. Must be the same shape as
         ``efficiencies``.
     priors : str or array_like, optional
-        Prior distribution to use in unfolding. If 'Jeffreys', then the
-        Jeffreys (flat) prior is used. Otherwise, must be array_like with
-        same shape as ``efficiencies`` (default is 'Jeffreys').
+        Prior distribution to use in unfolding. If 'Uniform', then the
+        Uniform (flat) prior is used. If 'Jeffreys', then the non-informative
+        Jeffreys' prior is used, requiring ``causes`` input. 
+        Otherwise, must be array_like with same shape as ``efficiencies`` 
+        (default is 'Uniform').
+    causes : array_like, optional
+        Cause bin center values for use in defining Jeffreys' prior.
     ts : {'ks', 'chi2', 'pf', 'rmd'}
         Name of test statistic to use for stopping condition (default is 'ks').
     ts_stopping : float, optional
@@ -108,9 +112,13 @@ def iterative_unfold(data=None, data_err=None, response=None,
     num_causes = len(efficiencies)
 
     # Setup prior
+    if causes not None:
+        causes = cast_to_array(causes)
+
     n_c = setup_prior(priors=priors,
                       num_causes=num_causes,
-                      num_observations=np.sum(data))
+                      num_observations=np.sum(data),
+                      cause_axis=causes)
 
     # Setup Mixer
     mixer = Mixer(data=data,